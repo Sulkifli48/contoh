ce20650db4e07f5ece09d162ca1b6d5d631cd9ee
Andi Amjad <andiamjad@gmail.com>
2024-10-28 15:16:40 +0800

    Aan 1

---
+++ frontend/src/components/sidebar/Sidebar.js
@@ -0,0 +1,110 @@
+import { useState } from 'react';
+import './Sidebar.css';
+import './Sidebar-category.css';
+import { Link, useNavigate } from 'react-router-dom';
+
+// import IconCS from '../../assets/img/support.png';
+
+export default function Sidebar() {
+  const name = sessionStorage.getItem("name");
+  const email = sessionStorage.getItem("email");
+  const [isOpen, setIsOpen] = useState(false);
+  const [loggingOut, setLoggingOut] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Menampilkan loading
+    setLoggingOut(true);
+
+    // Hapus informasi autentikasi dari sesi pengguna
+    sessionStorage.removeItem("user_id");
+    sessionStorage.removeItem("user_role");
+    sessionStorage.removeItem("name");
+    sessionStorage.removeItem("email");
+
+    setTimeout(() => {
+      // Sembunyikan loading
+      setLoggingOut(false);
+
+      // Redirect ke halaman login
+      if (!sessionStorage.getItem("user_role")) {
+        navigate("/");
+        window.location.reload();
+      }
+
+      console.log(`Good bye ${name}`);
+    }, 1000);
+  };
+
+  return (
+    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+      <div className="logo-details">
+        <div className="logo_name">BPKSDMD</div>
+        <i className='bx bx-menu' id="btn" onClick={toggleSidebar}></i>
+      </div>
+      <ul className="nav-list">
+        <li>
+          <Link to="/admin/dashboard">
+            <i className='bx bx-grid-alt'></i>
+            <span className="links_name">Dashboard</span>
+          </Link>
+          <span className="tooltip">Dashboard</span>
+        </li>
+        <li>
+          <Link to="/admin/add-admin">
+            <i class='bx bx-support'></i>
+            <span className="links_name">Admin</span>
+          </Link>
+          <span className="tooltip">Admin</span>
+        </li>
+        <li>
+          <Link to="/admin/add-dosen">
+            <i class='bx bxs-group'></i>
+            <span className="links_name">Dosen</span>
+          </Link>
+          <span className="tooltip">Dosen</span>
+        </li>
+        <li>
+          <Link to="/admin/add-matakuliah">
+            <i class='bx bxs-graduation'></i>
+            <span className="links_name">Matakuliah</span>
+          </Link>
+          <span className="tooltip">Matakuliah</span>
+        </li>
+        <li>
+          <Link to="/admin/add-ruangan">
+            <i class='bx bxs-home-smile'></i>
+            <span className="links_name">Ruangan</span>
+          </Link>
+          <span className="tooltip">Ruangan</span>
+        </li>
+        <li>
+          <Link to="/admin/jadwal">
+            <i class='bx bxs-time-five' ></i>
+            <span className="links_name">Jadwal</span>
+          </Link>
+          <span className="tooltip">Jadwal</span>
+        </li>
+
+        <li className="profile">
+          <div className="profile-details">
+            <div className="name_job">
+              <div className="name">{name}</div>
+              <div className="job">{email}</div>
+            </div>
+          </div>
+          {loggingOut ? (
+            <div>Loading...</div>
+          ) : (
+            <i className='bx bx-log-out' id="log_out" onClick={handleLogout}></i>
+          )}
+        </li>
+      </ul>
+    </div>
+  );
+}
--- frontend/src/pages/js/Admin.js
+++ frontend/src/pages/js/Admin.js
@@ -1,16 +1,42 @@
 // src/pages/AdminDashboard.js
 import React from 'react';
-import Sidebar from '../../components/Sidebar';
-import "../css/Admin.css"
+import Sidebar from '../../components/sidebar/Sidebar';
+
+import "../css/Admin.css";
+import IconStudy from '../../assets/img/study.png';
+import IconLecturer from '../../assets/img/lecturer.png';
+import IconBook from '../../assets/img/book.png';
+import IconClassroom from '../../assets/img/classroom.png';
+
+const cardsData = [
+  { name: 'Matakuliah', amount: 10, icon: IconStudy },
+  { name: 'Kelas', amount: 30, icon: IconBook },
+  { name: 'Dosen', amount: 20, icon: IconLecturer },
+  { name: 'Ruangan', amount: 30, icon: IconClassroom },
+];
+
+const Card = ({ name, amount, icon }) => (
+  <div className="card">
+    <img src={icon} alt={`${name} icon`} className='card-icon'/>
+    <h3>{name}</h3>
+    <p>{amount}</p>
+  </div>
+);
 
 const Admin = () => (
-    <div >
-        <Sidebar />
-        <div className="dashboard-content">
-            <h1>Admin Dashboard</h1>
-            {/* Konten lainnya dapat ditambahkan di sini */}
+  <div>
+    <Sidebar />
+    <div className='dashboard'>
+      <div className="dashboard-content">
+        <h1>Admin Dashboard</h1>
+        <div className="cards-container-db">
+          {cardsData.map((card, index) => (
+            <Card key={index} name={card.name} amount={card.amount} icon={card.icon}/>
+          ))}
         </div>
+      </div>
     </div>
+  </div>
 );
 
 export default Admin;
