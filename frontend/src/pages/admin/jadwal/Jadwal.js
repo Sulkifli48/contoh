4e8b9f587d46ab96026c7b7c11e379af66561aae
[Sulkifli] <[skifli2795@gmail.com]>
2024-12-14 17:44:35 +0800

    add and fix bug

--- frontend/src/pages/admin/jadwal/Jadwal.js
+++ frontend/src/pages/admin/jadwal/Jadwal.js
@@ -1,70 +1,98 @@
-import React, { useState} from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import axios from 'axios';
 import Sidebar from '../../../components/sidebar/Sidebar';
 import "./Style.css";
 import { MdDelete } from "react-icons/md";
-import { Box, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, Modal, TextField, Button, Grid, Dialog, DialogTitle, DialogContent, DialogActions, Autocomplete } from '@mui/material';
-import {  
+import {
+  Box,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  Button,
+  CircularProgress // import untuk indikator loading
+} from '@mui/material';
+import {
   MRT_GlobalFilterTextField as MRT_GLOBAL_FILTER_TEXT_FIELD,
-  MRT_TableBodyCellValue as MRT_TABLE_BODY_CELL_VALUE, 
+  MRT_TableBodyCellValue as MRT_TABLE_BODY_CELL_VALUE,
   MRT_TablePagination as MRT_TABLE_PAGINATION,
-  flexRender, 
-  useMaterialReactTable 
+  flexRender,
+  useMaterialReactTable
 } from 'material-react-table';
-import CloseIcon from '@mui/icons-material/Close';
-import { IconButton } from '@mui/material';
-import { initialMatakuliahs } from '../../../assets/mockdata/dataMatkul';
-import { initialDosen } from '../../../assets/mockdata/dataDosen';
 
 const Jadwal = () => {
-  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
   const [jadwalToDelete, setJadwalToDelete] = useState({ id: '', namaMatkul: '' });
   const [confirmationText, setConfirmationText] = useState('');
-  const [jadwalData, setJadwalData] = useState({
-    waktu: '',
-    kodeMatkul: '',
-    namaMatkul: '',
-    kelas: '',
-    wp: '',
-    namaDosen1: '',
-    namaDosen2: '',
-    kodeRuangan: '',
-  });
-
   const [jadwals, setJadwals] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
-  const handleOpenAddModal = () => {
-    setIsAddModalOpen(true);
-  };
+  const fetchScheduleData = useCallback(async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get('http://localhost:5000/api/jadwals1');
+      const mergedData = mergeScheduleData(response.data);
+      setJadwals(mergedData);
+    } catch (err) {
+      setError("Gagal mengambil data jadwal. Silakan coba lagi.");
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
 
-  const handleCloseAddModal = () => {
-    setIsAddModalOpen(false);
-  };
+  useEffect(() => {
+    fetchScheduleData();
+  }, [fetchScheduleData]);
 
-  const handleCloseDeleteDialog = () => {
-    setIsDeleteDialogOpen(false);
-  };
+  const mergeScheduleData = (jadwalsData) => {
+    const mergedSchedule = [];
+    jadwalsData.forEach((item) => {
+      const existingSchedule = mergedSchedule.find(
+        (mergedItem) =>
+          mergedItem.kelas === item.kelas &&
+          mergedItem.ruangan === item.ruangan &&
+          mergedItem.hari === item.hari
+      );
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setJadwalData({ ...jadwalData, [name]: value });
-  };
-
-  const handleAddJadwal = (e) => {
-    e.preventDefault();
-    setJadwals([{ ...jadwalData, id: Date.now() },...jadwals]);
-    setJadwalData({
-      waktu: '',
-      kodeMatkul: '',
-      namaMatkul: '',
-      kelas: '',
-      wp: '',
-      namaDosen1: '',
-      namaDosen2: '',
-      kodeRuangan: '',
+      if (existingSchedule) {
+        existingSchedule.jam.push(...item.jam);
+        existingSchedule.dosen = [...new Set([...existingSchedule.dosen, item.dosen])];
+      } else {
+        mergedSchedule.push({
+          ...item,
+          dosen: [item.dosen],
+          jam: item.jam,
+        });
+      }
     });
-    setIsAddModalOpen(false);
+
+    mergedSchedule.forEach((item) => {
+      if (item.jam.length > 0) {
+        const sortedTimes = item.jam.sort((a, b) => {
+          const [startA] = a.split(' - ');
+          const [startB] = b.split(' - ');
+          return startA.localeCompare(startB);
+        });
+
+        item.jam = `${sortedTimes[0].split(' - ')[0]} - ${sortedTimes[sortedTimes.length - 1].split(' - ')[1]}`;
+      }
+    });
+
+    return mergedSchedule;
   };
+
+  const handleCloseDeleteDialog = () => setIsDeleteDialogOpen(false);
 
   const handleDeleteJadwal = (jadwalId, nama) => {
     setJadwalToDelete({ id: jadwalId, nama });
@@ -72,22 +100,34 @@ const Jadwal = () => {
   };
 
   const handleConfirmDelete = () => {
-    setJadwals(jadwals.filter(jadwal => jadwal.id !== jadwalToDelete.id));
+    setJadwals(jadwals.filter((jadwal) => jadwal.id !== jadwalToDelete.id));
     setIsDeleteDialogOpen(false);
   };
 
   const columns = [
-    { accessorKey: 'waktu', header: 'Waktu (Jam)' },
-    { accessorKey: 'kodeMatkul', header: 'Kode Mata Kuliah' },
-    { accessorKey: 'namaMatkul', header: 'Nama Mata Kuliah' },
+    { accessorKey: 'hari', header: 'Hari' },
+    { accessorKey: 'jam', header: 'Jam' },
+    { accessorKey: 'matakuliah', header: 'Nama Mata Kuliah' },    
     { accessorKey: 'kelas', header: 'Kelas' },
-    { accessorKey: 'wp', header: 'W/P' },
-    { accessorKey: 'namaDosen1', header: 'Nama Dosen 1' },
-    { accessorKey: 'namaDosen2', header: 'Nama Dosen 2' },
-    { accessorKey: 'kodeRuangan', header: 'Kode Ruangan' },
+    { accessorKey: 'w/p', header: 'W/P' },
     {
-      accessorKey: "action",
-      header: "Action",
+      accessorKey: 'dosen',
+      header: 'Dosen',
+      Cell: ({ cell }) => (
+        <>
+          {cell.getValue().map((dosen, index) => (
+            <span key={index}>
+              {dosen}
+              {index < cell.getValue().length - 1 && <br />}
+            </span>
+          ))}
+        </>
+      )
+    },
+    { accessorKey: 'ruangan', header: 'Ruangan' },
+    {
+      accessorKey: 'action',
+      header: 'Action',
       Cell: ({ row }) => (
         <div>
           <MdDelete color='red' size={20} onClick={() => handleDeleteJadwal(row.original.id, row.original.namaMatkul)} />
@@ -105,7 +145,7 @@ const Jadwal = () => {
       showGlobalFilter: true,
     },
     muiPaginationProps: {
-      rowsPerPageOptions: [10, 15, 20, 25, 30, 35],
+      rowsPerPageOptions: [10, 15, 20, 25, 30],
       variant: 'outlined',
     },
     paginationDisplayMode: 'pages',
@@ -117,139 +157,69 @@ const Jadwal = () => {
       <div className='dashboard'>
         <div className='card-list-matakuliah'>
           <h1>Daftar Jadwal</h1>
-          <Stack>
-          <TableContainer className='border-list-matakuliah'>
-              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingBottom: '8px' }}>
-                <button
-                  style={{ width: '190px', height: '40px', background: '#3F72AF', borderRadius: '40px', color: 'white', fontWeight: 500 }}
-                  onClick={handleOpenAddModal}
-                >
-                  Add Jadwal
-                </button>
-                <MRT_GLOBAL_FILTER_TEXT_FIELD table={table} />
-              </Box>
-              <Table>
-                <TableHead className='border-botton'>
-                  {table.getHeaderGroups().map(headerGroup => (
-                    <TableRow key={headerGroup.id}>
-                      {headerGroup.headers.map(header => (
-                        <TableCell align="center" variant="head" key={header.id}>
-                          <span style={{ fontWeight: 'bold', fontSize: "15px" }}>
-                            {flexRender(header.column.columnDef.Header ?? header.column.columnDef.header, header.getContext())}
-                          </span>
-                        </TableCell>
-                      ))}
-                    </TableRow>
-                  ))}
-                </TableHead>
-                <TableBody>
-                  {table.getRowModel().rows.map(row => (
-                    <TableRow key={row.id}>
-                      {row.getVisibleCells().map(cell => (
-                        <TableCell align="center" key={cell.id}>
-                          <MRT_TABLE_BODY_CELL_VALUE cell={cell} table={table} />
-                        </TableCell>
-                      ))}
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <MRT_TABLE_PAGINATION table={table} />
-          </Stack>
+          {loading ? (
+            <div style={{ textAlign: 'center', padding: '20px' }}>
+              <CircularProgress />
+              <Typography variant="h6">Loading...</Typography>
+            </div>
+          ) : error ? (
+            <div style={{ textAlign: 'center', padding: '20px' }}>
+              <Typography color="error">{error}</Typography>
+            </div>
+          ) : (
+            <Stack>
+              <TableContainer className='border-list-matakuliah'>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingBottom: '8px' }}>
+                  <MRT_GLOBAL_FILTER_TEXT_FIELD table={table} />
+                </Box>
+                <Table>
+                  <TableHead className='border-botton'>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                      <TableRow key={headerGroup.id}>
+                        {headerGroup.headers.map((header) => (
+                          <TableCell align="center" variant="head" key={header.id}>
+                            <span style={{ fontWeight: 'bold', fontSize: '15px' }}>
+                              {flexRender(header.column.columnDef.header, header.getContext())}
+                            </span>
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    ))}
+                  </TableHead>
+                  <TableBody>
+                    {table.getRowModel().rows.map((row) => (
+                      <TableRow key={row.id}>
+                        {row.getVisibleCells().map((cell) => (
+                          <TableCell align="center" key={cell.id}>
+                            <MRT_TABLE_BODY_CELL_VALUE cell={cell} table={table} />
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <MRT_TABLE_PAGINATION table={table} />
+            </Stack>
+          )}
         </div>
 
         <Dialog open={isDeleteDialogOpen} onClose={handleCloseDeleteDialog}>
           <DialogTitle>Delete Jadwal</DialogTitle>
           <DialogContent>
-            <Typography className='dialog-delete'>apakah anda ingin menghapus "{jadwalToDelete.nama}"?</Typography>
-            <TextField fullWidth label="Confirmation Text" value={confirmationText} onChange={(e) => setConfirmationText(e.target.value)} />
+            <Typography>Apakah Anda ingin menghapus "{jadwalToDelete.nama}"?</Typography>
+            <TextField
+              fullWidth
+              label="Confirmation Text"
+              value={confirmationText}
+              onChange={(e) => setConfirmationText(e.target.value)}
+            />
           </DialogContent>
           <DialogActions>
             <Button onClick={handleCloseDeleteDialog}>Cancel</Button>
             <Button onClick={handleConfirmDelete}>Delete</Button>
           </DialogActions>
         </Dialog>
-
-        <Modal open={isAddModalOpen} onClose={handleCloseAddModal}>
-          <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', bgcolor: 'background.paper', boxShadow: 24, p: 4, width: 500 }}>
-            <Typography variant="h6">Add Jadwal
-              <IconButton onClick={handleCloseAddModal} style={{ float: 'right' }}><CloseIcon color='primary' /></IconButton>
-            </Typography>
-            <form onSubmit={handleAddJadwal}>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField fullWidth label="Waktu (Jam)" name="waktu" value={jadwalData.waktu} onChange={handleChange} required />
-                </Grid>
-                <Grid item xs={12}>
-                <Autocomplete
-                  options={initialMatakuliahs.map((jadwal) => `${jadwal.kode} - ${jadwal.matakuliah}`)}
-                  renderInput={(params) => (
-                    <TextField {...params} label="Kode - Mata Kuliah" name="kodeMatkul" required />
-                  )}
-                  value={jadwalData.kodeMatkul ? `${jadwalData.kodeMatkul} - ${jadwalData.namaMatkul}` : ''}
-                  onChange={(event, newValue) => {
-                    if (newValue) {
-                      const [kode, mataKuliah] = newValue.split(' - ');
-                      const selectedMatakuliah = initialMatakuliahs.find(matakuliah => matakuliah.kode === kode);
-                      const wpValue = selectedMatakuliah ? selectedMatakuliah.wp : '';
-                      setJadwalData({ 
-                        ...jadwalData, 
-                        kodeMatkul: kode, 
-                        namaMatkul: mataKuliah,
-                        wp: wpValue 
-                      });
-                    } else {
-                      setJadwalData({ 
-                        ...jadwalData, 
-                        kodeMatkul: '', 
-                        namaMatkul: '', 
-                        wp: '' 
-                      });
-                    }
-                  }}
-                  freeSolo
-                />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField fullWidth label="Kelas" name="kelas" value={jadwalData.kelas} onChange={handleChange} required />
-                </Grid>
-                <Grid item xs={12}>
-                  <Autocomplete
-                    options={initialDosen.map((dosen) => dosen.nama)}
-                    renderInput={(params) => (
-                      <TextField {...params} label="Nama Dosen 1" name="namaDosen1" required />
-                    )}
-                    value={jadwalData.namaDosen1}
-                    onChange={(event, newValue) => {
-                      setJadwalData({ ...jadwalData, namaDosen1: newValue });
-                    }}
-                    freeSolo
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Autocomplete
-                    options={initialDosen.map((dosen) => dosen.nama)}
-                    renderInput={(params) => (
-                      <TextField {...params} label="Nama Dosen 2" name="namaDosen2" />
-                    )}
-                    value={jadwalData.namaDosen2}
-                    onChange={(event, newValue) => {
-                      setJadwalData({ ...jadwalData, namaDosen2: newValue });
-                    }}
-                    freeSolo
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField fullWidth label="Kode Ruangan" name="kodeRuangan" value={jadwalData.kodeRuangan} onChange={handleChange} required />
-                </Grid>
-                <Grid item xs={12}>
-                  <Button type="submit" variant="contained" color="primary" fullWidth>Add Jadwal</Button>
-                </Grid>
-              </Grid>
-            </form>
-          </Box>
-        </Modal>
       </div>
     </div>
   );
